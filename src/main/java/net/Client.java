4f092e2114586ad1d6fe16db70416bbc88101032
Tzeentche <ilyasugako87@gmail.com>
2023-09-09 23:10:27 +0200

    Add realisation to Server

--- src/main/java/net/Client.java
+++ src/main/java/net/Client.java
@@ -2,6 +2,7 @@ package net;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 public class Client {
 
@@ -10,30 +11,6 @@ public class Client {
             SimpleClient sc = new SimpleClient();
             sc.start();
         }
-        
-        
-        sendRequest();
-    }
-
-    private static void sendRequest() throws IOException {
-
-        Socket socket = new Socket("127.0.0.1", 25225);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-        String sb = new String("Serhio");
-        bw.write(sb);
-        bw.newLine();
-        bw.flush();
-
-        String answer = br.readLine();
-        System.out.println("Client got string: " + answer);
-
-        br.close();
-        bw.close();
-
-        socket.close();
     }
 }
 
